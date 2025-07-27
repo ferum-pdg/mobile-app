056b6d1d626d43a552e062197a82204b259f1886
dariovas <dario.vasques@heig-vd.ch>
2025-07-27 16:18:00 +0200

    feat: add personal infos and goals

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -61,39 +61,112 @@ class ProfilePage extends StatelessWidget {
               const SizedBox(height: 12),
 
               const Text(
-                'Statistiques',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                'Informations personnelles',
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
               ),
 
-              Expanded(
-                child: GridView.count(
-                  crossAxisCount: 2,
-                  mainAxisSpacing: 16,
-                  crossAxisSpacing: 16,
-                  childAspectRatio: 1.2,
+              Container(
+                width: double.infinity,
+                margin: const EdgeInsets.symmetric(vertical: 20),
+                padding: const EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12),
+                  border: Border.all(color: Colors.black, width: 1.5),                                
+                ),
+                child: Column(
                   children: [
-                    Container(
-                    width: 50,
-                    height: 50,
-                    decoration: const BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.blue,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text('Âge', style: TextStyle(fontSize: 16, color: Colors.black)),
+                        Text('28 ans', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.black))
+                      ],
                     ),
-                    child: const Center(
-                      child: Icon(Icons.person, color: Colors.black),
+
+                    const SizedBox(height: 8),
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text('Poids', style: TextStyle(fontSize: 16, color: Colors.black)),
+                        Text('72 kg', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.black))
+                      ],
                     ),
+
+                    const SizedBox(height: 8),
+
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text('Taille', style: TextStyle(fontSize: 16, color: Colors.black)),
+                        Text('175 cm', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.black))
+                      ],
                     ),
-                    const SizedBox(width: 24),
-                    const Text(
-                      'Alex Dupont',
-                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+
+                    const SizedBox(height: 8),
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text('Niveau', style: TextStyle(fontSize: 16, color: Colors.black)),
+                        Text('Intermédiaire', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.black))
+                      ],
+                    )
+                  ],
+                ),
+              ),
+
+              const Text(
+                'Objectifs',
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              ),
+
+              Container(
+                width: double.infinity,
+                margin: const EdgeInsets.symmetric(vertical: 20),
+                padding: const EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12),
+                  border: Border.all(color: Colors.black, width: 1.5),                                
+                ),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text('Poids', style: TextStyle(fontSize: 16, color: Colors.black)),
+                        Text('70 kg', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.black))
+                      ],
+                    ),
+
+                    const SizedBox(height: 8),
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text('Dist hebdo', style: TextStyle(fontSize: 16, color: Colors.black)),
+                        Text('30 km', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.black))
+                      ],
+                    ),
+
+                    const SizedBox(height: 8),
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        Text('Fréquence', style: TextStyle(fontSize: 16, color: Colors.black)),
+                        Text('5 par sem', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.black))
+                      ],
                     ),
                   ],
-                )
+                ),
               )
-
             ],
-      ),
+        ),
+      )
     );
   }
 }
