2e2b7cebc5f79cef3d235a348961c117dfe60c40
Ewan Mariaux <ewan.mariaux@heig-vd,ch>
2025-08-25 18:58:00 +0200

    feat: create workoutLightClass

---
+++ lib/models/workout_light.dart
@@ -0,0 +1,69 @@
+import 'package:ferum/models/enum.dart';
+
+class WorkoutLightClass {
+  final String id;
+  final WorkoutSport sport;
+  final WorkoutType type;
+  final WorkoutStatut statut;
+  final String day;
+  final int duration;
+  final int week;
+  final WorkoutSource source;
+
+  WorkoutLightClass({
+    required this.id,
+    required this.sport,
+    required this.type,
+    required this.statut,
+    required this.day,
+    required this.duration,
+    required this.week,
+    required this.source,
+  });
+
+  factory WorkoutLightClass.fromJson(Map<String, dynamic> json) {
+    return WorkoutLightClass(
+      id: json['id'],
+      sport: WorkoutSport.values.firstWhere(
+        (e) => e.name == json['sport'],
+        orElse: () => throw Exception("Invalid sport: ${json['sport']}"),
+      ),
+      type: WorkoutType.values.firstWhere(
+        (e) => e.name == json['type'],
+        orElse: () => throw Exception("Invalid type: ${json['type']}"),
+      ),
+      statut: WorkoutStatut.values.firstWhere(
+        (e) => e.name == json['statut'],
+        orElse: () => throw Exception("Invalid statut: ${json['statut']}"),
+      ),
+      day: json['day'],
+      duration: json['duration'],
+      week: json['week'],
+      source: WorkoutSource.values.firstWhere(
+        (e) => e.name == json['source'],
+        orElse: () => throw Exception("Invalid source: ${json['source']}"),
+      ),
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'sport': sport.name,
+    'type': type.name,
+    'statut': statut.name,
+    'day': day,
+    'duration': duration,
+    'week': week,
+    'source': source.name,
+  };
+
+  static List<WorkoutLightClass> fromJsonList(List<dynamic> list) {
+    return list
+        .map((e) => WorkoutLightClass.fromJson(e as Map<String, dynamic>))
+        .toList();
+  }
+
+  static List<Map<String, dynamic>> toJsonList(List<WorkoutLightClass> items) {
+    return items.map((e) => e.toJson()).toList();
+  }
+}
