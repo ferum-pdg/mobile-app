583345fe994dfdf9f8309532c8931af769aebace
Ewan Mariaux <ewan.mariaux@heig-vd,ch>
2025-09-04 14:44:49 +0200

    fix: change fontsize

--- lib/pages/training_plan/intro_screen.dart
+++ lib/pages/training_plan/intro_screen.dart
@@ -9,15 +9,12 @@ import 'package:ferum/widgets/gradientButton.dart';
 
 import 'package:flutter/material.dart';
 
-/// IntroScreen widget: allows the user to go through multiple setup steps 
+/// IntroScreen widget: allows the user to go through multiple setup steps
 /// before generating a TrainingPlan.
 class IntroScreen extends StatefulWidget {
   final Function(TrainingPlan) onPlanCreated;
-  
-  const IntroScreen({
-    super.key,
-    required this.onPlanCreated,
-  });
+
+  const IntroScreen({super.key, required this.onPlanCreated});
 
   @override
   State<IntroScreen> createState() => _IntroScreenState();
@@ -27,12 +24,12 @@ class _IntroScreenState extends State<IntroScreen> {
   final PageController _pageController = PageController();
   // Tracks the current page index.
   int _currentPage = 0;
-  
+
   // The list of all setup screens.
   late final List<Widget> _pages;
 
   @override
-  void initState() {    
+  void initState() {
     super.initState();
     // Initialize the list of pages in the onboarding flow.
     _pages = [
@@ -41,37 +38,37 @@ class _IntroScreenState extends State<IntroScreen> {
       SwimmingScreen(),
       CyclingScreen(),
       DaysOfWeekScreen(),
-      EndDateScreen()
+      EndDateScreen(),
     ];
   }
 
   /// Navigate to the next page.
   /// If it's the last page, attempt to create the training plan.
   void _nextPage() async {
-    if (_currentPage < _pages.length - 1){
+    if (_currentPage < _pages.length - 1) {
       // Move to the next page with animation.
       _pageController.nextPage(
-        duration: const Duration(milliseconds: 300), 
+        duration: const Duration(milliseconds: 300),
         curve: Curves.easeOut,
       );
     } else {
-    // Last page: call the service to create the training plan.
-    try {
-      final plan = await TrainingPlanService().createTrainingPlan();
+      // Last page: call the service to create the training plan.
+      try {
+        final plan = await TrainingPlanService().createTrainingPlan();
 
-      if (plan != null) {
-        widget.onPlanCreated(plan);
-      } 
-    } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text(e.toString())),
-      );
+        if (plan != null) {
+          widget.onPlanCreated(plan);
+        }
+      } catch (e) {
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(SnackBar(content: Text(e.toString())));
+      }
     }
-  }
   }
 
   /// First screen displayed: introduction and motivation text.
-  Widget _welcomeScreen(){
+  Widget _welcomeScreen() {
     return Padding(
       padding: const EdgeInsets.all(32.0),
       child: Column(
@@ -81,38 +78,24 @@ class _IntroScreenState extends State<IntroScreen> {
           const SizedBox(height: 24),
           Text(
             "Bienvenue !",
-            style: TextStyle(
-              fontSize: 14,
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
-            ),
+            style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
           ),
           const SizedBox(height: 20),
           Text(
             "Ici vous pourrez créer un plan d'entraînement qui vous permettra d'atteindre des sommets!",
-            style: TextStyle(
-              fontSize: 14,
-              color: Colors.black,
-              fontWeight: FontWeight.bold, 
-            ),
+            style: TextStyle(fontSize: 19),
           ),
-          Image.asset(
-            "assets/img/icon-tp.png",
-            height: 400,
-          ),
+          Image.asset("assets/img/icon-tp.png", height: 350),
           const SizedBox(height: 20),
           Text(
             "Rejoins des centaines d'utilisateur-ices satisfait-e-s!",
-            style: TextStyle(
-              fontSize: 14,
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
-            ),            
-          ),          
+            style: TextStyle(fontSize: 18),
+          ),
         ],
       ),
     );
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -128,18 +111,20 @@ class _IntroScreenState extends State<IntroScreen> {
                 });
               },
               children: _pages,
-            )
+            ),
           ),
-          
+
           // Bottom button to go to the next page or create the plan.
           Padding(
             padding: const EdgeInsets.all(16.0),
             child: GradientButton(
-              text: _currentPage < _pages.length - 1 ? "Suivant" : "Je crée mon plan", 
+              text: _currentPage < _pages.length - 1
+                  ? "Suivant"
+                  : "Je crée mon plan",
               onTap: _nextPage,
               height: 60.0,
             ),
-          )
+          ),
         ],
       ),
     );
