6057f173800d0d75aa0b1ca6be530fa51b109a67
Ewan Mariaux <ewan.mariaux@heig-vd,ch>
2025-09-04 19:03:39 +0200

    fix: change backend URL

--- lib/utils/sharedPreferences.dart
+++ lib/utils/sharedPreferences.dart
@@ -6,6 +6,6 @@ Future<void> defaultSharedPreferences(SharedPreferences prefs) async {
   await prefs.setDouble('height', 180);
   await prefs.setDouble('weight', 70);
   await prefs.setBool('hasTrainingPlan', false);
-  await prefs.setString('BackendURL', 'http://83.228.200.235/backend/');
+  await prefs.setString('BackendURL', 'http://83.228.200.235');
   await prefs.setBool('isAuthentified', false);
 }
